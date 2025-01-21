f0b70a2740e7108c250fe07f9279670e37f23292
KKJ1000 <kkj1733n@gmail.com>
2025-01-21 22:05:26 +0900

    [Feat] 백준 - 11022번

---
+++ Baekjoon/반복문/백준 - 11022번.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+//두 정수 A와 B를 입력받은 다음, A + B를 출력하는 프로그램을 작성하시오.
+//첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+//각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+// A+B를 아름답게 출력하는 문제
+//각 테스트 케이스마다 "Case #x: A + B = C" 형식으로 출력한다.x는 테스트 케이스 번호이고 1부터 시작하며,
+//C는 A + B이다.
+int main()
+{
+	int T = 0;
+	int a = 0;
+	int b = 0;
+
+	cin >> T;
+	for (int i = 0; i < T; i++)
+	{
+		cin >> a >> b;
+		cout << "Case #" << i + 1 << ": " << a << " + " << b << " = " << a + b << endl;
+	}
+	return 0;
+}
