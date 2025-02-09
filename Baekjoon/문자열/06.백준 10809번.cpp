846246e9a9d7372a8dacde76099e6dc553c66c32
KKJ1000 <kkj1733n@gmail.com>
2025-02-09 20:26:33 +0900

    [Fix] 백준 - 10809번 수정

--- Baekjoon/문자열/06.백준 10809번.cpp
+++ Baekjoon/문자열/06.백준 10809번.cpp
@@ -1,31 +1,30 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-// 첫째 줄에 단어 S가 주어진다. 단어의 길이는 100을 넘지 않으며, 알파벳 소문자로만 이루어져 있다.
-// 이것도 아스키 코드 쓸까? 
-// 97 ~ 122가 a 부터 z까지이다.
+// 알파벳 소문자로만 이루어진 단어 S가 주어진다. 각각의 알파벳에 대해서, 단어에 포함되어 있는 경우에는 처음 등장하는 위치를,
+// 포함되어 있지 않은 경우에는 -1을 출력하는 프로그램을 작성.
+
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
 
-    char word[101] = {};
-	int flag[130];
+	char word[101] = {};
+	int flag[26];
 	cin >> word;
-	
-	for (int i = 97; i <= 122; i++)
+
+	for (int i = 0; i < 26; i++)
 	{
 		flag[i] = -1;
 	}
 
 	for (int i = 0; i < 101; i++)
 	{
-		for (int j = 97; j <= 122; j++)
+		for (int j = 0; j < 26; j++)
 		{
 			if (flag[j] == -1)
 			{
-				if (word[i] == j)
+				if (word[i] == j + 'a')
 				{
 					flag[j] = i;
 				}
@@ -36,11 +35,10 @@ int main()
 			break;
 	}
 
-	for (int i = 97; i <= 122; i++)
+	for (int i = 0; i < 26; i++)
 	{
 		cout << flag[i] << " ";
 	}
 
-
-    return 0;
+	return 0;
 }
