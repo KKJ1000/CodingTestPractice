5974c0cdba4e46f093560acffdf0b2d5716f8039
KKJ1000 <kkj1733n@gmail.com>
2025-02-13 22:27:28 +0900

    [Feat] 백준 - 25083번

---
+++ Baekjoon/심화/01.백준 25083번.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// 새싹을 출력하는 문제
+
+int main()
+{
+	cout << "         ,r'\"7" << endl;
+	cout << "r`-_   ,'  ,/" << endl;
+	cout << " \\. \". L_r'" << endl;
+	cout << "   `~\\/" << endl;
+	cout << "      |" << endl;
+	cout << "      |" << endl;
+	return 0;
+}
